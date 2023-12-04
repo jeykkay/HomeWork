a4abae7ccd3b0dc830af8345968fa6aa3e7764b3
evgeniy <evgeniy.sumar.smr@gmail.com>
2023-12-04 20:26:45 +0300

    users_reg

--- homework12/class.py
+++ homework12/class.py
@@ -2,35 +2,50 @@ class Moving:
     def move(self):
         raise NotImplemented
 
+
 class Animal(Moving):
     def voice(self):
         raise NotImplemented
+
 
 class Transport(Moving):
     def launch(self):
         raise NotImplemented
 
+
 class Duck(Animal):
     def voice(self):
         return 'quack'
+
     def move(self):
         return 'swim'
+
+
 class Tiger(Animal):
     def voice(self):
         return 'roar'
+
     def move(self):
         return 'runs'
 
+
 class Car(Transport):
+    def __init__(self, status='not_started'):
+        self.status = status
+
     def move(self):
-        return 'drives'
+        if self.status == 'not_started':
+            return 'drives'
+        return 'car is not started'
+
     def launch(self):
-        return 'starts'
+        self.status = 'started'
+        return 'launch'
 
 
-duck=Duck()
-tiger=Tiger()
-car=Car()
+duck = Duck()
+tiger = Tiger()
+car = Car()
 
 print(duck.move(), duck.voice())
 print(tiger.move(), tiger.voice())
