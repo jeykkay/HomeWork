7744a6d17286c94c701f1032890195e21b2177d5
jeykkay <evgeniy.sumar.smr@gmail.com>
2023-12-05 19:02:39 +0300

    qwe

---
+++ homework12.py
@@ -0,0 +1,52 @@
+class Moving:
+    def move(self):
+        raise NotImplemented
+
+
+class Animal(Moving):
+    def voice(self):
+        raise NotImplemented
+
+
+class Transport(Moving):
+    def launch(self):
+        raise NotImplemented
+
+
+class Duck(Animal):
+    def voice(self):
+        return 'quack'
+
+    def move(self):
+        return 'swim'
+
+
+class Tiger(Animal):
+    def voice(self):
+        return 'roar'
+
+    def move(self):
+        return 'runs'
+
+
+class Car(Transport):
+    def __init__(self):
+        self.status = 'not_started'
+
+    def launch(self):
+        if self.status == 'started':
+            return 'launch'
+        return 'car is not started'
+
+    def move(self):
+        self.status = 'started'
+        return 'starts'
+
+
+duck = Duck()
+tiger = Tiger()
+car = Car()
+
+print(duck.move(), duck.voice())
+print(tiger.move(), tiger.voice())
+print(car.move(), car.launch())
