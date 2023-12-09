e9550cd3739edf90a52dd6abf837a1c68df31104
evgeniy <evgeniy.sumar.smr@gmail.com>
2023-12-09 20:05:30 +0300

    DataStorage

--- lesson13/data_storage.py
+++ lesson13/data_storage.py
@@ -45,6 +45,6 @@ class DataStorageWrite(DataStorage):
 dsw = DataStorageWrite('my.json')
 dsw.connect()
 print(dsw.content)
-dsw.append('some')
+dsw.append('some text')
 dsw.disconnect()
 dsw.connect()
