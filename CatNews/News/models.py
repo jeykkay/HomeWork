29fc68871710916a1b2b3dbb5cdcf15600bab42e
Swoosh1k <lilsmosh228@gmail.com>
2024-01-25 18:47:59 +0300

    class23_rework

--- CatNews/News/models.py
+++ CatNews/News/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 
 class News(models.Model):
-    image = models.ImageField(upload_to='...')
+    image = models.ImageField(upload_to='cat_news/images/', blank=True)
     title = models.CharField(max_length=128, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
     rating = models.FloatField(default=0)
     date = models.DateField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    comments = models.TextField()
 
     def __str__(self):
         return self.title
@@ -20,4 +22,4 @@ class Comment(models.Model):
     date = models.DateField(auto_now_add=True)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
-    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comments_news')
+    news = models.ForeignKey('News', on_delete=models.CASCADE, related_name='comments_news')
